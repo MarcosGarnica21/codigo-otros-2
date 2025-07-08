8ee1849c27305c3822fe28e5f4571fda95663a12
MarcosGarnica21 <garnicar_@hotmail.com>
2025-07-07 18:24:54 -0600

    Modified html and js

--- formulario.js
+++ formulario.js
@@ -1,32 +1,39 @@
 var formulario = document.querySelector("#form")
-
+var lista = document.querySelector("#lista-de-invitados") //selecciona el contenedor del html que agregué
 formulario.onsubmit = function(e) {
 
-  e.prevent();
+e.preventDefault(); //Corrección de sintaxis, evita que se reformule al recargar
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+var n = document.getElementById("name"); //cambien la sintaxis y vamos a acceder por su ID
+var eInput = document.getElementById("age"); //coreccion de e a e input
+var na = document.getElementById("nationality"); 
 
-  var nombre = n.value
-  var edad = e.value
+var nombre = n.value;
+var edad = (parseInt(eInput.value)); //que lo convierte a numero cuando ingrese
+  //var i = na.selectedIndex
+var nacionalidad = na.options[na.selectedIndex].value;
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
   console.log(nombre, edad)
   console.log(nacionalidad)
 
+  var hayErrores = false; // se necesita para controlar el if
+
   if (nombre.length === 0) {
     n.classList.add("error")
+    hayErrores = true;
+  } else {
+    n.classList.remove("eror"); //si ya tiene error lo quitas
   }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+  if (isNaN(edad) || edad < 18 || edad > 120) { //corrección de sintaxis
+    eInput.classList.add("error"); //usar eInput
+    hayErrores = true;
+  } else {
+    eInput.classList.remove("eror");
   }
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
+  if (!hayErrorer){//si no hay error se agrega a lista
+    agregarInvitado(nombre, edad, nacionalidad);
+    formulario.rese(); //si todo es correcto reseta el formulario
   }
 }
 
@@ -38,7 +45,7 @@ document.body.appendChild(corteLinea)
 document.body.appendChild(botonBorrar);
 
 function agregarInvitado(nombre, edad, nacionalidad) {
-
+/*
   if (nacionalidad === "ar") {
     nacionalidad = "Argentina"
   }
@@ -51,16 +58,25 @@ function agregarInvitado(nombre, edad, nacionalidad) {
   else if (nacionalidad === "per") {
     nacionalidad = "Peruana"
   }
+ */
+//Mejor crear un objeto
+  const nacionalidades ={ 
+    ar: "Argentina",
+    mx: "Mexicana",
+    vnzl: "Venezolana",
+    per: "Peruana",
+  };
 
-var lista = document.getElementById("lista-de-invitados")
+//var lista = document.getElementById("lista-de-invitados")
 
 var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
+elementoLista.classList.add("elemento-lista") //corección sintaxis de added
 lista.appendChild(elementoLista)
 
 var spanNombre = document.createElement("span")
 var inputNombre = document.createElement("input")
 var espacio = document.createElement("br")
+
 spanNombre.textContent = "Nombre: "
 inputNombre.value = nombre 
 elementoLista.appendChild(spanNombre)
